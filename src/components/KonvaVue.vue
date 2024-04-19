2b6b23169eb82f34eb64f2c3343c6b553e2b6a2c
zhangke0524 <1091723881@qq.com>
2024-04-19 23:07:22 +0800

    style

--- src/components/KonvaVue.vue
+++ src/components/KonvaVue.vue
@@ -5,14 +5,20 @@
     <div class="tool-area">
       <h2>工具栏</h2>
       <p>这里是工具栏</p>
-      <div v-for="item in toolList">
-        <div :class="{
-          'rect-tool-icon': item.id === 'rect',
-          'circle-tool-icon': item.id === 'circle',
-          'line-tool-icon': item.id === 'line',
-          'label-tool-icon': item.id === 'label',
-          'custom-tool-icon': item.id === 'custom',
-          }" :title="item.name">
+      <div class="tool-list">
+        <div
+          v-for="(item, index) in toolList"
+          :key="index"
+          :title="item.name"
+          class="tool-icon"
+          :class="{
+            'rect-tool-icon': item.id === 'rect',
+            'circle-tool-icon': item.id === 'circle',
+            'line-tool-icon': item.id === 'line',
+            'label-tool-icon': item.id === 'label',
+            'custom-tool-icon': item.id === 'custom',
+          }"
+        >
         </div>
       </div>
     </div>
@@ -74,25 +80,63 @@ onMounted(() => {
   height: calc(100% - 40px);
   display: flex;
   justify-content: space-between;
-}
-.container {
-  width: calc(100% - 500px);
-  height: 100%;
-  background-color: rgb(253, 223, 228);
-}
-.tool-area {
-  width: 500px;
-  background-color: rgb(207, 227, 245);
-}
-.rect-tool-icon {
-  width: 30px;
-  height: 30px;
-  border: 1px solid black;
-}
-.circle-tool-icon {
-  width: 30px;
-  height: 30px;
-  border: 1px solid black;
-  border-radius: 50%;
+  .container {
+    width: calc(100% - 500px);
+    height: 100%;
+    background-color: rgb(253, 223, 228);
+  }
+  .tool-area {
+    width: 500px;
+    background-color: rgb(207, 227, 245);
+    .tool-list {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .tool-icon {
+        margin: 10px;
+        cursor: pointer;
+      }
+      .rect-tool-icon {
+        width: 30px;
+        height: 30px;
+        border: 1px solid black;
+      }
+      .circle-tool-icon {
+        width: 30px;
+        height: 30px;
+        border: 1px solid black;
+        border-radius: 50%;
+      }
+      .line-tool-icon::before {
+        content: '';
+        display: block;
+        width: 30px;
+        height: 1px;
+        background-color: black;
+      }
+      .triangle-tool-icon {
+        width: 0;
+        height: 0;
+        border-left: 15px solid transparent;
+        border-right: 15px solid transparent;
+        border-bottom: 30px solid black;
+      }
+      .label-tool-icon {
+        display: inline-block;
+        padding: 5px 10px;
+        border: 1px solid black;
+        border-radius: 10px;
+        font-family: Arial, sans-serif;
+        font-size: 14px;
+        color: #333;
+      }
+      .custom-tool-icon {
+        width: 30px;
+        height: 30px;
+        background-color: #7FFFD4; /* 设置背景颜色 */
+        clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%); /* 使用多边形裁剪 */
+      }
+    }
+  }
 }
 </style>
